5841f008c9cac047b87d0ab8abd40d8963ac57a7
Tim Schindler <xh3b4sd@gmail.com>
2017-02-22 17:27:52 +0100

    removed cluster spec directory which is now provided by the clusterspec repo

--- spec/node/node.go
+++
@@ -1,6 +0,0 @@
-package node
-
-type Node struct {
-	Memory string `json:"memory"`
-	CPUs   int    `json:"cpus"`
-}
--- spec/operator/operator.go
+++
@@ -1,8 +0,0 @@
-package operator
-
-type Operator struct {
-	CertctlVersion      string `json:"certctlVersion"`
-	K8sVmVersion        string `json:"k8sVmVersion"`
-	KubectlVersion      string `json:"kubectlVersion"`
-	NetworkSetupVersion string `json:"networkSetupVersion"`
-}
