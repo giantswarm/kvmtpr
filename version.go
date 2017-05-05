1ee19af3bc24cfc498f8a71b3aad190d23863890
Ross Fairbanks <rossf7@users.noreply.github.com>
2017-05-05 14:58:45 +0200

    Fix TPR version which doesn’t have the kvm prefix (#26)

--- version.go
+++ version.go
@@ -2,5 +2,5 @@ package kvmtpr
 
 const (
 	// VersionV1 is the Kubernetes API version for v1 cluster resources.
-	VersionV1 = "kvm.cluster.giantswarm.io/v1"
+	VersionV1 = "cluster.giantswarm.io/v1"
 )
